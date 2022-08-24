dac1af978e76feba52b48f9f94e2a3fdbcece073
nukrisnamurti <ruciera@gmail.com>
2022-08-24 14:30:20 +0700

    edit API

---
+++ src/data/product.js
@@ -0,0 +1,69 @@
+export const getProduct = () => {
+  const product = [
+    {
+      id: "1",
+      productname: "Geki Geki",
+      price: "19.99",
+      description:
+        "Geki-dan's art about a lonely girl at train terminal waiting for someone to approach her",
+      image: "./product/geki.jpg",
+    },
+    {
+      id: "2",
+      productname: "Mika Pikazo",
+      price: "19.99",
+      description: "みかだよ",
+      image: "./product/pokazo.jpeg",
+    },
+    {
+      id: "3",
+      productname: "Makima",
+      price: "19.99",
+      description: "まきまさん",
+      image: "./product/makima.jfif",
+    },
+    {
+      id: "4",
+      productname: "Granblue Fantasy",
+      price: "19.99",
+      description: "Granblue Fantasy is a mobage created by Cygames.",
+      image: "./product/gbf.png",
+    },
+    {
+      id: "5",
+      productname: "Yoneyama Mai",
+      price: "19.99",
+      description: "Stare to the sky, find trueself",
+      image: "./product/yoneya.jpeg",
+    },
+    {
+      id: "6",
+      productname: "Scenery-I",
+      price: "19.99",
+      description: "A lovely day",
+      image: "./product/soo3.jpg",
+    },
+    {
+      id: "7",
+      productname: "Scenery-II",
+      price: "19.99",
+      description: "Classroom",
+      image: "./product/soo4.jpg",
+    },
+    {
+      id: "8",
+      productname: "Scenery-III",
+      price: "19.99",
+      description: "Lovely day in Japan",
+      image: "./product/soo2.jpg",
+    },
+    {
+      id: "9",
+      productname: "Scenery-IV",
+      price: "19.99",
+      description: "A cat in the abandoned house",
+      image: "./product/geki.jpg",
+    },
+  ];
+  return product;
+};
